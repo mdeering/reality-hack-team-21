fe12411089a22f0900f38382ce8d88cb51645e8a
Jo Chung <chjoanna@163.com>
2025-01-24 18:49:28 -0500

    Enabled color passthrough

---
+++ RealityHackTeamCanada/Assets/_RGBTBD/01_Scripts/EnvRaycastManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Meta.XR;
+
+public class EnvRaycastManager : MonoBehaviour
+{
+    [SerializeField] private GameObject sphere;
+    [SerializeField] private EnvironmentRaycastManager environmentRaycastManager;
+
+    private void Update()
+    {
+        //environmentRaycastManager.Raycast
+
+        //sphere.position = EnvironmentRaycastHit.point
+        //move to hit point
+    }
+}
