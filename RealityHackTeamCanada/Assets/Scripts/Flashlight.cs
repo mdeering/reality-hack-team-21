708db897772753fc17583c6fdc6d5c98dc8c808c
Michael Deering <mdeering@mdeering.com>
2025-01-24 13:35:18 -0500

    Stubbed out flashlight prefab

---
+++ RealityHackTeamCanada/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,34 @@
+using Meta.XR.ImmersiveDebugger;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] private GameObject _lightActiveVisual;
+    
+    [DebugMember(Category = "Flashlight")]
+    [SerializeField] private bool _isOn;
+
+    void Start()
+    {
+        _lightActiveVisual.SetActive(_isOn);
+    }
+
+    [DebugMember(Category = "Flashlight")]
+    [Button("💡 Turn On")]
+    public void TurnOn()
+    {
+        _lightActiveVisual.SetActive(true);
+        _isOn = true;
+        Debug.Log("Flashlight is on");
+    }
+
+    [DebugMember(Category = "Flashlight")]
+    [Button("🔦 Turn Off")]
+    public void TurnOff()
+    {
+        _lightActiveVisual.SetActive(false);
+        _isOn = false;
+        Debug.Log("Flashlight is off");
+    }
+}
