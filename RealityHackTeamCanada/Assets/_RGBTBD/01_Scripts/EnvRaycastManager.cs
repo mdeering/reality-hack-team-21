9e5e280d9ac75bc30c1a559f5329b6e771c7255f
Jo Chung <chjoanna@163.com>
2025-01-25 11:43:07 -0500

    Changed build settings

--- RealityHackTeamCanada/Assets/_RGBTBD/01_Scripts/EnvRaycastManager.cs
+++ RealityHackTeamCanada/Assets/_RGBTBD/01_Scripts/EnvRaycastManager.cs
@@ -4,7 +4,7 @@ using Meta.XR;
 public class EnvRaycastManager : MonoBehaviour
 {
     [SerializeField] private GameObject sphere;
-    [SerializeField] private EnvironmentRaycastManager environmentRaycastManager;
+    [SerializeField] private EnvironmentRaycastManager raycastManager;
 
     private void Update()
     {
@@ -12,5 +12,31 @@ public class EnvRaycastManager : MonoBehaviour
 
         //sphere.position = EnvironmentRaycastHit.point
         //move to hit point
+        //moveObject();
     }
+    /*
+    private void moveObject()
+    {
+        // Cast a ray from the center of the camera's view
+        Ray ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+
+        // Perform raycast using EnvironmentRaycastManager
+        if (raycastManager.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            // Move the object to the hit point
+            sphere.transform.position = hit.point;
+
+            // Optionally, align the object's rotation to the hit normal
+            sphere.transform.rotation = Quaternion.LookRotation(hit.normal);
+
+            Debug.Log("Object moved to: " + hit.point);
+        }
+        else
+        {
+            Debug.Log("No hit detected.");
+        }
+    
+    }
+    */
 }
